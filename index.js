e27fd48ba488b00b5514c1c009aefc1b5cb22a86
Christophe Garconnet <iMac@iMac-de-Christophe.local>
2015-10-15 23:55:09 +0200

    end of lesson 4

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ $(document).ready(function () {
     // $(".xxx") for a Class
     $(".add-items").submit(function(event){
 
-//    	alert('cool');
-	console.log("Hi")
-
 	event.preventDefault(); // submit will submit the form and load again the page, this is why without this code, the Hi will only falsh and desiappear because a new page is loaded
+//    	alert('cool');
+//	console.log($("#todo-list-item").val());
+
+// Now let's save the reference of our input value
+
+	var item = $("#todo-list-item").val();
+
+//	Var xx is local variable
+//	xx = will be a global variable
 
     });
 });
