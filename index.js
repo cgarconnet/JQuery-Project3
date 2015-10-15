41dd1e951ffc500d680203d6270f605d5b79b19b
Christophe Garconnet <iMac@iMac-de-Christophe.local>
2015-10-15 23:47:22 +0200

    a Hi showing each time without flashin thanks to event.preventDefault

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
     // YOUR CODE HERE!
+    // $("#xxx") for a Id
+    // $(".xxx") for a Class
+    $(".add-items").submit(function(event){
+
+//    	alert('cool');
+	console.log("Hi")
+
+	event.preventDefault(); // submit will submit the form and load again the page, this is why without this code, the Hi will only falsh and desiappear because a new page is loaded
+
+    });
 });
